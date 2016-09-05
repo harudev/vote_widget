11edd885a8b483d842a48b26d854e97a2cb5e6f4
ruci06 <ruci06@gmail.com>
2016-09-05 19:51:15 +0900

    Revert "Mysql 쿼리 파일 추가"

--- sql/allmovies.sql
+++
@@ -1,2 +0,0 @@
--- Movies table에 있는 모든 영화의 “영화 제목”, “좋아하는 사람 수" 를 보여주는 쿼리
-select title, vote_count from movies;
--- sql/mostlike.sql
+++
@@ -1,2 +0,0 @@
--- 사용자들이 가장 좋아하는 영화
-select * from movies order by vote_count desc limit 1;
